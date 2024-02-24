a9cd1547e8f384eca3f6c7f2e86be5ca6295de25
no-la <nzyc0216@icloud.com>
2024-02-25 00:23:10 +0900

    Add osa_k

---
+++ algorithm/osa_k/osa_k.py
@@ -0,0 +1,24 @@
+# エラトステネスの篩の強化版
+MAXN = 10**6+10
+sieve = [i for i in range(MAXN)] # sieve[i]: iの最も小さい素因数
+p = 2
+while p*p<=MAXN:
+    if sieve[p]==p:
+        for q in range(p*p, MAXN, p):
+            if sieve[q]==q:
+                sieve[q] = p
+    p += 1
+
+from collections import defaultdict
+def prime_factors(n):
+    """n(>=2)の素因数を返す"""
+    d = defaultdict(int) # 適宜変更
+    while n>1:
+        d[sieve[n]] += 1
+        n //= sieve[n]
+    return d
+
+
+if __name__ == '__main__':
+    for i in range(2, 31):
+        print(i, prime_factors(i))
