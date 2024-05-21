705aeeb3d2bab78e745e8b23abebbcb5f5b35f45
no-la <nzyc0216@icloud.com>
2024-05-21 15:42:51 +0900

    abc247E

--- contest/abc247E/abc247E.1.py
+++ contest/abc247E/abc247E.1.py
@@ -8,11 +8,42 @@ A = list(map(int, input().split()))
 
 
 # Xより大きいもの、Yより小さいものを含まない最大の部分列
-# を調べて、その中にある最も近いX, Yの位置がわかればいい
-l, r = 0, 0
-xpos = []
-ypos = []
-while r<=N:
-    while X<=A[r]<=Y:
-        ...
+# ごとに調べればいい
 
+d = [i for i in range(N) if A[i]>X or A[i]<Y] # 条件を満たさない点
+d.append(N)
+# print(d)
+# 尺取り法
+ans = 0
+right = 0
+has_X, has_Y = 0, 0
+ll = -1
+for rr in d:
+    # print("rr", rr)
+    for left in range(ll+1, rr):
+        while right<rr and (not has_X or not has_Y):
+            if A[right]==X:
+                has_X += 1
+            if A[right]==Y:
+                has_Y += 1
+
+            right += 1
+            
+        if has_X and has_Y:
+            # print((ll, rr), (left, right), (has_X, has_Y))
+            ans += rr-(right-1)
+            
+        if left == right: # [left, right)が空集合なので'temp=初期値'になっているはず
+            right += 1
+        else: #このあとleftを+1するので、その分の情報を削る
+            if A[left]==X:
+                has_X -= 1
+                # print("del X")
+            if A[left]==Y:
+                has_Y -= 1
+                # print("del Y")
+    ll = rr
+            
+            
+
+print(ans)
