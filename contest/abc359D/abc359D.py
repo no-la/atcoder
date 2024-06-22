646b97a02383bfcfed28293a06518b0d87b8b4fa
no-la <nzyc0216@icloud.com>
2024-06-22 23:39:29 +0900

    save abc359D.py

--- contest/abc359D/abc359D.py
+++ contest/abc359D/abc359D.py
@@ -8,3 +8,17 @@ for i in range(1, N+1):
 # count[i]: S[:i]にある?の個数
 print(count)
 
+ans = 0
+temp = 1
+for i in range(N-K+1):
+    # S[i:i+K]が回文になる場合の数
+    c = 0
+    s = S[i:i+K]
+    for j in range(K):
+        if s[j]!="?" and s[-j-1]!="?" and s[j]!=s[-j-1]:
+            break
+        
+    else:
+        temp = (temp*count[i+K])
+        continue
+    
