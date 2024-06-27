c96cb81a238dfd21e02016e2a5fce0cfd16d3fbc
no-la <nzyc0216@icloud.com>
2024-06-27 17:23:19 +0900

    abc273D

---
+++ contest/abc273D/abc273D.1.py
@@ -0,0 +1,69 @@
+from collections import defaultdict
+H, W, *pos = map(int, input().split())
+N = int(input())
+columns = defaultdict(list)
+rows = defaultdict(list)
+for _ in range(N):
+    r, c = map(int, input().split())
+    columns[r].append(c)
+    rows[c].append(r)
+Q = int(input())
+
+# ソート
+for k in columns:
+    columns[k].sort()
+for k in rows:
+    rows[k].sort()
+
+# 二分探索で一番近い壁を探せばいい
+import bisect
+# 基本的にbisect_leftを使う
+# 渡す配列は昇順(reverse=False)ソートしておく
+for _ in range(Q):
+    d, l = input().split()
+    l = int(l)
+    if d=="L":
+        i, j = 0, 1
+        tar = columns[pos[i]]
+        from_ = pos[j]
+        nearest_wall_id = bisect.bisect_left(tar, from_)-1
+        to = max(1, from_-l)
+        if nearest_wall_id<0 or tar[nearest_wall_id]<to:
+            pos[j] = to
+        else:
+            pos[j] = tar[nearest_wall_id]+1
+    elif d=="R":
+        i, j = 0, 1
+        tar = columns[pos[i]]
+        from_ = pos[j]
+        nearest_wall_id = bisect.bisect_left(tar, from_)
+        to = min(W, from_+l)
+        if nearest_wall_id>=len(tar) or to<tar[nearest_wall_id]:
+            pos[j] = to
+        else:
+            pos[j] = tar[nearest_wall_id]-1
+    elif d=="U":
+        i, j = 1, 0
+        tar = rows[pos[i]]
+        from_ = pos[j]
+        nearest_wall_id = bisect.bisect_left(tar, from_)-1
+        to = max(1, from_-l)
+        if nearest_wall_id<0 or tar[nearest_wall_id]<to:
+            pos[j] = to
+        else:
+            pos[j] = tar[nearest_wall_id]+1
+    else:
+        i, j = 1, 0
+        tar = rows[pos[i]]
+        from_ = pos[j]
+        nearest_wall_id = bisect.bisect_left(tar, from_)
+        to = min(H, from_+l)
+        if nearest_wall_id>=len(tar) or to<tar[nearest_wall_id]:
+            pos[j] = to
+        else:
+            pos[j] = tar[nearest_wall_id]-1
+    
+    # print(*pos, "wall=", tar[nearest_wall_id] if 0<=nearest_wall_id<len(tar) else '-', "tar=", tar)
+    print(*pos)
+        
+
---
+++ contest/abc273D/abc273D.py
@@ -0,0 +1,72 @@
+from collections import defaultdict
+H, W, *pos = map(int, input().split())
+N = int(input())
+columns = defaultdict(list)
+rows = defaultdict(list)
+for _ in range(N):
+    r, c = map(int, input().split())
+    columns[r].append(c)
+    rows[c].append(r)
+Q = int(input())
+
+# ソート
+for k in columns:
+    columns[k].sort()
+for k in rows:
+    rows[k].sort()
+
+# 二分探索で一番近い壁を探せばいい
+import bisect
+# 基本的にbisect_leftを使う
+# 渡す配列は昇順(reverse=False)ソートしておく
+for _ in range(Q):
+    d, l = input().split()
+    l = int(l)
+    if d=="L":
+        i, j = 0, 1
+        tar = columns[pos[i]]
+        from_ = pos[j]
+        nearest_wall_id = bisect.bisect_left(tar, from_)-1
+        to = from_-l
+        if nearest_wall_id<0 or tar[nearest_wall_id]<to:
+            pos[j] = max(1, to)
+        else:
+            pos[j] = tar[nearest_wall_id]+1
+    elif d=="R":
+        i, j = 0, 1
+        tar = columns[pos[i]]
+        from_ = pos[j]
+        nearest_wall_id = bisect.bisect_left(tar, from_)
+        to = from_+l
+        if nearest_wall_id>=len(tar) or to<tar[nearest_wall_id]:
+            pos[j] = min(W, to)
+        else:
+            pos[j] = tar[nearest_wall_id]-1
+    elif d=="U":
+        i, j = 1, 0
+        tar = rows[pos[i]]
+        from_ = pos[j]
+        nearest_wall_id = bisect.bisect_left(tar, from_)-1
+        to = from_-l
+        if nearest_wall_id<0 or tar[nearest_wall_id]<to:
+            pos[j] = max(1, to)
+        else:
+            pos[j] = tar[nearest_wall_id]+1
+    else:
+        i, j = 1, 0
+        tar = rows[pos[i]]
+        from_ = pos[j]
+        nearest_wall_id = bisect.bisect_left(tar, from_)
+        to = from_+l
+        if nearest_wall_id>=len(tar) or tar[nearest_wall_id]<to:
+            pos[j] = min(H, to)
+        else:
+            pos[j] = tar[nearest_wall_id]-1
+
+    pos[0] = min(H, max(1, pos[0]))
+    pos[1] = min(W, max(1, pos[1]))
+    
+    # print(*pos, "wall=", tar[nearest_wall_id] if 0<=nearest_wall_id<len(tar) else '-', "tar=", tar)
+    print(*pos)
+        
+
