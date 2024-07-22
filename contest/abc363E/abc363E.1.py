629101a7e710df83d4eedb223035b3cdf42ed9e9
no-la <nzyc0216@icloud.com>
2024-07-22 17:10:22 +0900

    abc363E

--- contest/abc363E/abc363E.1.py
+++ contest/abc363E/abc363E.1.py
@@ -5,19 +5,21 @@ from heapq import heapify, heappop, heappush
 #heapify(a:list)
 #heappop(a) (最小値)
 #heappush(a, value)
-hq = []
+hq = [] # 海面に面している区画のキュー
 seen = [[False]*W for _ in range(H)]
 heapify(hq)
 for i in range(H):
     heappush(hq, (A[i][0], i, 0))
-    heappush(hq, (A[i][W-1], i, W-1))
     seen[i][0] = True
-    seen[i][W-1] = True
+    if not seen[i][W-1]:
+        heappush(hq, (A[i][W-1], i, W-1))
+        seen[i][W-1] = True
 for j in range(1, W-1):
     heappush(hq, (A[0][j], 0, j))
-    heappush(hq, (A[H-1][j], H-1, j))
     seen[0][j] = True
-    seen[H-1][j] = True
+    if not seen[H-1][j]:
+        heappush(hq, (A[H-1][j], H-1, j))
+        seen[H-1][j] = True
 
 ans = H*W
 for y in range(1, Y+1):
