4568aa35478865751bc3d51a870c3ea4024bc747
no-la <nzyc0216@icloud.com>
2024-11-05 19:00:03 +0900

    abc229D

--- contest/abc229D/abc229D.py
+++ contest/abc229D/abc229D.py
@@ -1,30 +1,21 @@
 S = input()
 K = int(input())
+N = len(S)
 
-b = False
-d = [] #.のみからなる部分文字列がS[d[_][0]:d[_][1]]になる
-for i in range(len(S)):
-    if S[i]==".":
-        if not b:
-            d.append([i])
-            b = True
-    else:
-        if b:
-            d[-1].append(i)
-            b = False
-if b:
-    d[-1].append(i+1)
-    
+# .がK個以下である最大の区間
+
+# 尺取り法
 ans = 0
-s = 0
-count = d[s][1]-d[s][0]
-for i in range(1, len(d)):
-    l = d[i][1]-d[i][0]
-    if count+l>K:
-        ans = max(ans, s+d[i][0]+K-count)
-        s = d[i+1][0]
-    else:
-        ans = max(ans, i-s)
-        
+right = 0
+count = 0
+for left in range(N):
+    while right < N and count + (S[right] == ".") <= K:
+        count += S[right] == "."
+        right += 1
+    ans = max(ans, right - left)
+    if left == right:  # [left, right)が空集合なので'temp=初期値'になっているはず
+        right += 1
+    else:  # このあとleftを+1するので、その分の情報を削る
+        count -= S[left] == "."
 
-    
+print(ans)
