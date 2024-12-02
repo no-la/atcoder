5ed85dfe437bdc3a4776456e47bcd1e066f7f5c9
no-la <nzyc0216@icloud.com>
2024-12-02 17:54:33 +0900

    abc382E

---
+++ contest/abc382E/abc382E.2.py
@@ -0,0 +1,28 @@
+import math
+
+N, X = map(int, input().split())
+P = list(map(lambda x: int(x) / 100, input().split()))
+INF = 10**15
+
+q = [[0] * (N + 1) for _ in range(N + 1)]
+# q[i][j]: 1回の操作でP[:i]からレアカードをj枚手に入れる確率
+q[0][0] = 1
+for i in range(N):
+    for j in range(N):
+        q[i + 1][j] += q[i][j] * (1 - P[i])
+        q[i + 1][j + 1] += q[i][j] * P[i]
+
+# print(*q, sep="\n")
+Q = q[-1]
+# Q[i]: 1回の操作でi枚のレアカードを手に入れる確率
+
+dp = [INF] * (X + 1)
+# dp[i]: i枚手に入れるときの操作回数の期待値
+dp[0] = 0
+for i in range(1, X + 1):
+    temp = 0
+    for j in range(1, N + 1):
+        temp += dp[max(0, i - j)] * Q[j]
+    dp[i] = (1 + temp) / (1 - Q[0])
+
+print(dp[X])
