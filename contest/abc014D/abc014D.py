0909e97235245c31663919751770b63b4e8f984d
nola <nzyc0216@icloud.com>
2025-01-27 10:00:35 +0000

    change to not using pip but pypy3 -m pip and add powershell

--- contest/abc014D/abc014D.py
+++ contest/abc014D/abc014D.py
@@ -1,5 +1,7 @@
 import sys
+
 sys.setrecursionlimit(100000)
+
 
 class SegTree:
     def __init__(self, num, e, operator=max):
@@ -9,70 +11,72 @@ class SegTree:
             e : 単位元(初期値)
             operator : 2つの子ノードの値から親ノードの値を作る関数
         """
-        #self.tree[i+self.offset]:要素iのみの区間の値
-        self.offset = 1 << (num-1).bit_length()
-        self.tree = [e]*(self.offset<<1)
+        # self.tree[i+self.offset]:要素iのみの区間の値
+        self.offset = 1 << (num - 1).bit_length()
+        self.tree = [e] * (self.offset << 1)
         self.e = e
         self.operator = operator
-    
+
     def update(self, i, value):
-        """i番目の要素をvalueにする
-        """
+        """i番目の要素をvalueにする"""
         i += self.offset
         self.tree[i] = value
-        #親ノードを順に変更していく
-        while i>1:
-            v1, v2 = self.tree[i], self.tree[i^1] #子ノード
-            i >>= 1 #iを親ノードへずらす
+        # 親ノードを順に変更していく
+        while i > 1:
+            v1, v2 = self.tree[i], self.tree[i ^ 1]  # 子ノード
+            i >>= 1  # iを親ノードへずらす
             self.tree[i] = self.operator(v1, v2)
-            
+
     def get_point(self, p):
-        """p番目の要素を取得する
-        """
-        return self.tree[p+self.offset]
-    
+        """p番目の要素を取得する"""
+        return self.tree[p + self.offset]
+
     def get_range(self, l, r):
-        """区間[l, r)の値を取得する
-        """
+        """区間[l, r)の値を取得する"""
         l += self.offset
         r += self.offset
-        
+
         ans = self.e
-        while l<r:
-            if r&1:
-                ans = self.operator(ans, self.tree[r-1])
+        while l < r:
+            if r & 1:
+                ans = self.operator(ans, self.tree[r - 1])
                 r -= 1
-            if l&1:
+            if l & 1:
                 ans = self.operator(ans, self.tree[l])
                 l += 1
             l >>= 1
             r >>= 1
         return ans
 
+
 N = int(input())
 from collections import defaultdict
+
 E = defaultdict(list)
-for _ in range(N-1):
-    x, y = map(lambda x: int(x)-1, input().split())
+for _ in range(N - 1):
+    x, y = map(lambda x: int(x) - 1, input().split())
     E[x].append(y)
     E[y].append(x)
 
 euler_tour = []
-first_appear = [None]*N
+first_appear = [None] * N
 
 ROOT = 0
-seen = [False]*N
+seen = [False] * N
+
+
 def dfs(d, v):
     if first_appear[v] is None:
         first_appear[v] = len(euler_tour)
     euler_tour.append((d, v))
-    
+
     for w in E[v]:
         if seen[w]:
             continue
         seen[w] = True
-        dfs(d+1, w)
+        dfs(d + 1, w)
         euler_tour.append((d, v))
+
 
 seen[ROOT] = True
 dfs(0, ROOT)
@@ -84,12 +88,12 @@ for i in range(len(euler_tour)):
 
 Q = int(input())
 for _ in range(Q):
-    a, b = map(lambda x: int(x)-1, input().split())
+    a, b = map(lambda x: int(x) - 1, input().split())
     ai, bi = first_appear[a], first_appear[b]
-    if ai>bi:
+    if ai > bi:
         ai, bi = bi, ai
-    lca = st.get_range(ai, bi+1)
-    ans = euler_tour[ai][0] + euler_tour[bi][0] - 2*lca[0] + 1
+    lca = st.get_range(ai, bi + 1)
+    ans = euler_tour[ai][0] + euler_tour[bi][0] - 2 * lca[0] + 1
     # print(a, b, lca)
     print(ans)
 
