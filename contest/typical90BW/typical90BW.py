465010872f18c693383f68f4785278d8a1feb05e
no-la <nzyc0216@icloud.com>
2025-03-17 21:46:07 +0900

    typical90BW

--- contest/typical90BW/typical90BW.py
+++ contest/typical90BW/typical90BW.py
@@ -3,12 +3,16 @@ import sys
 input = lambda: sys.stdin.readline().rstrip()
 N = int(input())
 
-count = -1
+
+count = 0
 x = N
-for i in range(2, 10**7):
+for i in range(2, 10**6 + 10):
     while x > 1 and x % i == 0:
         count += 1
         x //= i
 
-ans = count.bit_length()
+if x > 1:
+    count += 1
+
+ans = (count - 1).bit_length()
 print(ans)
