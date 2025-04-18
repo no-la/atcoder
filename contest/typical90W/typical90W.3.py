bc179db20a484a8da60936ab2d12060c50021316
nola <nzyc0216@icloud.com>
2025-04-18 03:07:21 +0000

    typical90W syokadai2

---
+++ contest/typical90W/typical90W.3.py
@@ -0,0 +1,45 @@
+"""小課題3 TLE"""
+
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+H, W = map(int, input().split())
+C = [input() for _ in range(H)]
+N = 2 ** (W + 1)
+MOD = 10**9 + 7
+
+if H > 9 or W > 9:
+    print("MURI☆")
+    exit()
+
+
+def can_put(k, x):
+    i, j = divmod(k - 1, W)
+    if i == 0:
+        return j == 0 or x & (1 << W) == 0
+    if j == 0:
+        return x & 2 == 0 and x & 4 == 0
+    if j == W - 1:
+        return not (x & 1 or x & 2 or x & (1 << W))
+
+    return not (x & 1 or x & 2 or x & 4 or x & (1 << W))
+
+
+dp = [[0] * N for _ in range(H * W + 1)]
+dp[0][0] = 1
+# dp[k][x]: マス k を、直前のW+1マスの状態が x として見たときの場合の数
+# x はbitで持つ
+
+for k in range(H * W):
+    for x in range(N):
+        i, j = divmod(k, W)
+        # マス k+1 に置くとき
+        if C[i][j] == "." and can_put(k + 1, x):
+            dp[k + 1][(x >> 1) + (1 << W)] += dp[k][x]
+            dp[k + 1][(x >> 1) + (1 << W)] %= MOD
+
+        # マス k+1 に置かないとき
+        dp[k + 1][x >> 1] += dp[k][x]
+        dp[k + 1][x >> 1] %= MOD
+
+print(sum(dp[H * W]) % MOD)
